387f35b6198aa79ddd1963185ddc2f8e5bc4666d
gmo9-mrfool <85418772+gmo9-mrfool@users.noreply.github.com>
2021-06-19 23:50:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import score
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('SNAKE GAME')
 screen.tracer(0)
 
-snake =Snake()
+snake = Snake()
+food = Food()
+scoreboard = score()
 
 screen.listen()
-screen.onkey(snake.up,'Up')
-screen.onkey(snake.down,'Down')
-screen.onkey(snake.left,'Left')
-screen.onkey(snake.right,'Right')
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 game_is_on = True
 while game_is_on:
     screen.update()
@@ -21,5 +26,21 @@ while game_is_on:
 
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+# Detect collision with tail
+for segment in snake.parts[1:]:
+
+    if snake.head.distance(segment) < 10:
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
